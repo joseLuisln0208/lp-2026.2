20ec8479f510f9cba7fc475975be93471eb9641f
jose.luis <joseluisln0208@gmail.com>
2026-08-31 11:35:34 -0300

    Adicionei um teste simples de idade

---
+++ src/main/java/Oi.java
@@ -0,0 +1,16 @@
+public class Oi
+{
+    public int idade = 0;
+
+    public void main(String[] args)
+    {
+        if(idade >= 18)
+        {
+            System.out.print("Você é maior de idade");
+        }
+        else
+        {
+            System.out.print("Você é menor de idade");
+        }
+    }
+}
